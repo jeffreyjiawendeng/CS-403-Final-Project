aabe16c6448eb06d58db1b0aa792724bfced59e1
r3pu1s3 <jshong@umass.edu>
2025-11-26 21:38:43 -0500

    tried lqr

--- CompetitionCodes/InvertedPendulumCompetition/YourControlCode.py
+++ CompetitionCodes/InvertedPendulumCompetition/YourControlCode.py
@@ -1,94 +1,86 @@
-import mujoco
-from stable_baselines3 import SAC
 import os
+os.environ["MKL_THREADING_LAYER"] = "GNU"
+# from stable_baselines3 import SAC
+
 import numpy as np
+from scipy.linalg import solve_discrete_are
+import mujoco
+
+  
 class YourCtrl:
   def __init__(self, m:mujoco.MjModel, d: mujoco.MjData):
     # model
-    self.m = m
+    self.model = m
     # data
-    self.d = d
+    self.data = d
     
-
-    # e = myenv(self.m, self.d, render_mode=None)
-    # initial positions
-    self.init_qpos = d.qpos.copy()
-    self.model = SAC.load("SAC_hp_obs_reward4")
-    # Control gains (using similar values to CircularMotion)
-    # ??????
-    # self.kp = 70.0
-    # self.kd = 3.0
-  
     
+    # actuator_moment = np.zeros((self.m.nu, self.m.nv))
+    # mujoco.mju_sparse2dense(
+    # actuator_moment,
+    # self.d.actuator_moment.reshape(-1),
+    # self.d.moment_rownnz,
+    # self.d.moment_rowadr,
+    # self.d.moment_colind.reshape(-1),
+    # )
+    # mujoco.mj_forward(self.model, self.data)
+    self.data.qacc = 0  # Assert that there is no the acceleration.
+    mujoco.mj_inverse(self.model, self.data)
+    # print(self.data.qfrc_inverse)
+    
+    # may have to save a specific value
+    self.initial_qpos = self.data.qpos.copy()
+    
+    # the force we want to stabilize I think
+    
+    # may have to save a specific value
+    self.force = self.data.qfrc_inverse.copy()
+    
+    
+    self.data.ctrl = self.force[:6]
+    self.data.qpos = self.initial_qpos
+    
+    # gets A and B values
+    A = np.zeros((2*self.model.nv, 2*self.model.nv))
+    B = np.zeros((2*self.model.nv, self.model.nu))
+    epsilon = 1e-6
+    flg_centered = True
+    mujoco.mjd_transitionFD(self.model, self.data, epsilon, flg_centered, A, B, None, None)
+    
+    nu =self.model.nu  # Alias for the number of actuators.
+    R = np.eye(nu)
+    
+    # Q = np.eye(2*self.model.nv)
+    # 1, 3, 4
+    Q = np.diag([10,100,10,100,100,10,100, 1, 1, 1, 1, 1, 1, 1])
+    # print(self.model.nv)
+    
+    # different Q values
+    # cost should be 
+    
+    
+    # Solve discrete Riccati equation.
+    P = solve_discrete_are(A, B, Q, R)
 
+    # Compute the feedback gain matrix K.
+    self.K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+    # print(self.initial_qpos)
+    
+    # may have to reset with initialization
+    mujoco.mj_resetData(self.model, self.data)
+
+   
   def CtrlUpdate(self):
-    # unit of control is based on gear, but it looks like torque
-    # if RL/ML, we can think of action space be be 6 numbers in R. so output would be R^6
-    # input, can be orientation (and maybe velocity) of each part + what torque each joint feels (this would require sensors, so not likely)
-    # what do we include in state space?
-    # print(self.d.qpos)
-    # for i in range(6):
-    q = self.d.qpos[:6]
-    dq = self.d.qvel[:6]
-    ee_bid = self.m.body('EE_Frame').id
-    ee_pos = self.d.body(ee_bid).xpos.copy()      # (3,)
-    ee_quat = self.d.body(ee_bid).xquat.copy()     # (4,)
-    vel = np.zeros(6)
-    mujoco.mj_objectVelocity(self.m, self.d, mujoco.mjtObj.mjOBJ_BODY, ee_bid, vel, flg_local=False)
-    ee_lin_vel = vel[:3]
-    ee_ang_vel = vel[3:]
-    
-    pend_jid = self.m.joint('pend_roll').id
-    theta     = self.d.joint(pend_jid).qpos
-    sin_theta =np.sin(theta)
-    cos_theta = np.cos(theta)
+    # self.data.qacc = 0  # Assert that there is no the acceleration.
 
-    theta_dot = self.d.joint(pend_jid).qvel
-    pend_bid = self.m.body('pendulum').id
-    pend_pos = self.d.body(pend_bid).xpos.copy()
-
-    ee_to_pendulum = pend_pos - ee_pos
-
-    obs = np.concatenate(
-        [q, 
-          dq,
-          ee_pos,
-          ee_quat,
-          ee_lin_vel,
-          ee_ang_vel,
-          sin_theta,
-          cos_theta,
-          theta_dot,
-          pend_pos,
-          ee_to_pendulum
-          ], 
-        dtype=np.float32)      
-    
-    # self.d.ctrl[i] = self.kp*(self.init_qpos[i] - self.d.qpos[i])  + self.kd*(-1*self.d.qvel[i])
-      # self.d.ctrl[2] = self.kp*(self.init_qpos[2] - self.d.qpos[2])  + self.kd*(-1*self.d.qvel[2])
-      
-      
-    action, _ = self.model.predict(obs)
-    max_ctrl = self.m.actuator_ctrlrange[:, 1]
-  
-    # may have to adjust for scaling
-    scaled = action * max_ctrl
-    # print(action)
-    for i in range(6):
-      self.d.ctrl[i] = scaled[i]
-    # self.d.ctrl[0] = -1* self.d.qfrc_applied[0]
-    # self.d.ctrl[1] = -1*self.d.qfrc_applied[1]
-    # self.d.ctrl[4] = 10
-    # print(self.d.qfrc_applied)
+    dq = np.zeros(self.model.nv)
+    mujoco.mj_differentiatePos(self.model, dq, 1, self.initial_qpos, self.data.qpos)
+    dx = np.hstack((dq, self.data.qvel)).T
+    self.data.ctrl = self.force[:6] - self.K @ dx
+    # self.data.ctrl = np.clip(self.force[:6] - self.K @ dx,
+    #                      self.model.actuator_ctrlrange[:,0],
+    #                      self.model.actuator_ctrlrange[:,1])
+    # self.data.ctrl[:] = self.force[:6]
     return True 
 
-
-# some questions before answering:
-# what should be the action space of a policy? - R^6, real number value representing torque squashed in appropriate range
-# what should be a state space of a policy? --> can be a lot of things, will have to prob experiment with them
-  # makes most sense to have orientation (in quaternion) and position () and qfrc_inverse (or actuator_force and qfrc_applied)
-  # in total this would be 
-# is my environment broken? --> no, you may have to adjust it for reset
-# we can do either RL or some complex dynamics solution
-# for dynamics solution, theoretically you can just move to counteract the applied force/torque, but the issue is reaction time, machine has to be one step ahead
 
